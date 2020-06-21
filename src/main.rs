2469dd3350654850ce95d8a97deeab9665bc3c3d
Walther <veeti.haapsamo@gmail.com>
2020-06-21 03:15:19 +0300

    Chapter 5: surface normals

--- src/main.rs
+++ src/main.rs
@@ -28,12 +28,16 @@ impl Ray {
     fn color(&self) -> Rgb<u8> {
         // Chapter 4: sphere and background
         let color: Vec3;
-        let sphere = Sphere::new(Vector3::new(0.0, 0.0, -1.0), 0.5);
+        let sphere_center: Vec3 = Vector3::new(0.0, 0.0, -1.0);
+        let sphere_radius = 0.5;
+        let sphere = Sphere::new(sphere_center, sphere_radius);
         let unit_direction: Unit<Vec3> = Unit::new_normalize(self.direction);
 
-        if sphere.hit(&self) {
-            // Sphere, red
-            color = Vec3::new(1.0, 0.0, 0.0);
+        if let Some(distance) = sphere.hit(&self) {
+            // Sphere
+            let normal = self.point_at_parameter(distance) - sphere_center;
+            let normal = normal.normalize();
+            color = 0.5 * Vec3::new(normal.x + 1.0, normal.y + 1.0, normal.z + 1.0);
         } else {
             // Background, blue-white gradient. Magic from tutorial.
             let t = 0.5 * (unit_direction.y + 1.0);
@@ -58,16 +62,22 @@ impl Sphere {
         Sphere { center, radius }
     }
 
-    fn hit(&self, ray: &Ray) -> bool {
+    /// Calculate whether the ray hits the sphere. If, return the distance from ray origin to the hitpoint. If not, return `None`
+    fn hit(&self, ray: &Ray) -> Option<Float> {
         let oc = ray.origin - self.center;
         let a = ray.direction.dot(&ray.direction);
         let b = 2.0 * oc.dot(&ray.direction);
         let c = oc.dot(&oc) - self.radius * self.radius;
         let discriminant = b * b - 4.0 * a * c;
-        discriminant > 0.0
+        if discriminant < 0.0 {
+            None
+        } else {
+            Some((-b - discriminant.sqrt()) / (2.0 * a))
+        }
     }
 }
 
+/// The main drawing function, returns an `ImageResult`.
 fn draw() -> ImageResult<()> {
     // Let's start dirty & hardcoded
     let width = 2000;
