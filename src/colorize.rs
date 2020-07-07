83d1a4bf1ab73b0dff50dda40a32e6bd3669605b
Walther <veeti.haapsamo@gmail.com>
2020-07-07 03:27:32 +0300

    Clean up colorize a bit

--- src/colorize.rs
+++ src/colorize.rs
@@ -62,6 +62,7 @@ pub fn colorize(
                                 );
                         }
                         crate::materials::MaterialType::Diffuse => {
+                            // Use a probability density function to figure out where to scatter a new ray
                             let light_ptr =
                                 HitablePDF::new(Arc::clone(&lights), hit_record.position);
                             let mixture_pdf = MixturePDF::new(light_ptr, scatter_record.pdf_ptr);
@@ -70,6 +71,18 @@ pub fn colorize(
                                 Ray::new(hit_record.position, mixture_pdf.generate(rng), ray.time);
                             let pdf_val = mixture_pdf.value(scattered.direction, ray.time, rng);
 
+                            // recurse
+                            let recurse = colorize(
+                                &scattered,
+                                background_color,
+                                world,
+                                lights,
+                                depth + 1,
+                                max_depth,
+                                rng,
+                            );
+
+                            // Blend it all together
                             return emitted
                                 + scatter_record.attenuation
                                     * hit_record.material.scattering_pdf(
@@ -78,93 +91,12 @@ pub fn colorize(
                                         &scattered,
                                         rng,
                                     )
-                                    * colorize(
-                                        &scattered,
-                                        background_color,
-                                        world,
-                                        lights,
-                                        depth + 1,
-                                        max_depth,
-                                        rng,
-                                    )
+                                    * recurse
                                     / pdf_val;
                         }
                     }
                 }
             }
-        } // BACKUP
-          // // Hit an object
-          // Some(hit_record) => {
-          //     let emitted: Color = hit_record.material.emit(
-          //         ray,
-          //         &hit_record,
-          //         hit_record.u,
-          //         hit_record.v,
-          //         hit_record.position,
-          //     );
-          //     // Try to scatter and colorize the new ray
-          //     match hit_record.material.scatter(&ray, &hit_record, rng) {
-          //         // Got a scatter
-          //         Some(scatter_record) => {
-          //             // If material is specular, cast a specular ray
-          //             match scatter_record.material_type {
-          //                 crate::materials::MaterialType::Specular => {
-          //                     return scatter_record.attenuation
-          //                         * colorize(
-          //                             &scatter_record.specular_ray.unwrap(),
-          //                             background_color,
-          //                             world,
-          //                             lights,
-          //                             depth + 1,
-          //                             max_depth,
-          //                             rng,
-          //                         );
-          //                 }
-          //                 _ => (),
-          //             }
-
-          //             // Compute a probability density function value; where to scatter?
-
-          //             let light_ptr = HitablePDF::new(Arc::clone(&lights), hit_record.position);
-          //             let mixture_pdf = MixturePDF::new(light_ptr, scatter_record.pdf_ptr);
-
-          //             let scattered =
-          //                 Ray::new(hit_record.position, mixture_pdf.generate(rng), ray.time);
-          //             let pdf_val = mixture_pdf.value(scattered.direction, ray.time, rng);
-
-          //             // Recurse; compute the color of the scattered ray
-          //             let recurse = colorize(
-          //                 &scattered,
-          //                 background_color,
-          //                 world,
-          //                 lights,
-          //                 depth + 1,
-          //                 max_depth,
-          //                 rng,
-          //             );
-
-          //             // Put it all together:
-          //             let color: Color = emitted
-          //                 + scatter_record.attenuation
-          //                     * hit_record
-          //                         .material
-          //                         .scattering_pdf(ray, &hit_record, &scattered, rng)
-          //                     * recurse //NOTE:  Should this _really_ be multiplication here?
-          //                     / pdf_val;
-
-          //             return color;
-          //         }
-          //         // No scatter, emit only
-          //         None => {
-          //             return emitted;
-          //         }
-          //     }
-          // }
-          // // Did not hit anything, return the background_color
-          // None => {
-          //     // DEBUG
-          //     // return Color::new(0.3, 0.0, 0.0);
-          //     return background_color;
-          // }
+        }
     }
 }
