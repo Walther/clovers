8f9a4cbeec1e5795b314886263f9bb622f757e6e
Walther <veeti.haapsamo@gmail.com>
2020-12-13 15:55:20 +0200

    Add default() for Lambertian

--- src/materials/lambertian.rs
+++ src/materials/lambertian.rs
@@ -1,7 +1,22 @@
 use super::{Material, MaterialType, ScatterRecord};
-use crate::{hitable::HitRecord, pdf::CosinePDF, ray::Ray, textures::Texture, Float, PI};
+use crate::{
+    color::Color,
+    hitable::HitRecord,
+    pdf::CosinePDF,
+    ray::Ray,
+    textures::{SolidColor, Texture},
+    Float, PI,
+};
 use rand::prelude::ThreadRng;
 use serde::{Deserialize, Serialize};
+
+impl Default for Lambertian {
+    fn default() -> Self {
+        Lambertian {
+            albedo: SolidColor::new(Color::default()),
+        }
+    }
+}
 
 #[derive(Copy, Clone, Deserialize, Serialize, Debug)]
 pub struct Lambertian {
