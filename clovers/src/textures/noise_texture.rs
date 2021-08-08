f5b163fadabada3bec736b71df381c477b449e66
Walther <veeti.haapsamo@gmail.com>
2021-08-09 01:57:43 +0300

    Document NoiseTexture

--- clovers/src/textures/noise_texture.rs
+++ clovers/src/textures/noise_texture.rs
@@ -3,7 +3,9 @@ use crate::{color::Color, perlin::Perlin, Float, Vec3};
 use serde::{Deserialize, Serialize};
 
 // TODO: This might be currently oddly broken and resulting in overflowy surfaces
+// TODO: better documentation
 #[derive(Copy, Clone, Deserialize, Serialize, Debug)]
+/// A NoiseTexture object.
 pub struct NoiseTexture {
     #[serde(skip)]
     noise: Perlin,
@@ -11,11 +13,14 @@ pub struct NoiseTexture {
 }
 
 impl NoiseTexture {
+    /// Creates a new NoiseTexture object with the specified Perlin noise and scaling factor.
     pub fn new(noise: Perlin, scale: Float) -> Texture {
         Texture::NoiseTexture(NoiseTexture { noise, scale })
     }
 
     // TODO: investigate why this does not swirl as well as the example marble in tutorial
+    // TODO: spatial vs surface noise alternatives?
+    /// Evaluates the color at the given spatial coordinate.
     pub fn color(self, _u: Float, _v: Float, position: Vec3) -> Color {
         let depth = 7;
         Color::new(1.0, 1.0, 1.0)
