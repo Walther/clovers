7ba100aece363fc8c96c8b75dc41a04dc9be8ace
Walther <veeti.haapsamo@gmail.com>
2021-08-08 19:21:53 +0300

    Ray: derive Debug, PartialEq

--- clovers/src/ray.rs
+++ clovers/src/ray.rs
@@ -3,7 +3,7 @@
 use crate::{Float, Vec3};
 
 /// A Ray has an origin and a direction, as well as an instant in time it exists in. Motion blur is achieved by creating multiple rays with slightly different times.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Ray {
     pub origin: Vec3,
     pub direction: Vec3,
