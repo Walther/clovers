b05cf4c1e7d092af35f0afb71e2426af31624969
Walther <veeti.haapsamo@gmail.com>
2021-08-21 22:02:48 +0300

    WIP: further attempts at using the GPU pipeline

--- clovers/src/draw_gpu.rs
+++ clovers/src/draw_gpu.rs
@@ -9,6 +9,8 @@
 // TODO: temporary during development
 #![allow(dead_code)]
 
+use std::{borrow::Cow, path::PathBuf};
+
 use clovers::{color::Color, scenes::Scene, Float};
 use spirv_std::glam::{vec4, Vec4};
 
@@ -35,15 +37,13 @@ pub fn main_vs(
 
 #[cfg(not(any(target_os = "android", target_arch = "wasm32")))]
 mod shaders {
-    // The usual usecase of code generation is always building in build.rs, and so the codegen
-    // always happens. However, we want to both test code generation (on android) and runtime
-    // compilation (on desktop), so manually fill in what would have been codegenned for desktop.
     #[allow(non_upper_case_globals)]
     pub const main_fs: &str = "main_fs";
     #[allow(non_upper_case_globals)]
     pub const main_vs: &str = "main_vs";
 }
 
+// TODO: borrowed from rust-gpu
 fn create_pipeline(
     device: &wgpu::Device,
     pipeline_layout: &wgpu::PipelineLayout,
@@ -86,9 +86,10 @@ fn create_pipeline(
     })
 }
 
-// use bytemuck::{Pod, Zeroable};
-// #[derive(Copy, Clone, Pod, Zeroable)]
-#[derive(Copy, Clone)]
+// END borrowed from rust-gpu
+
+use bytemuck::{Pod, Zeroable};
+#[derive(Copy, Clone, Pod, Zeroable)]
 #[repr(C)]
 struct ShaderConstants {
     pub width: u32,
@@ -102,8 +103,8 @@ struct ShaderConstants {
 pub async fn draw(
     width: u32,
     height: u32,
-    _samples: u32,
-    _max_depth: u32,
+    samples: u32,
+    max_depth: u32,
     _gamma: Float,
     _quiet: bool,
     _scene: Scene,
@@ -145,11 +146,90 @@ pub async fn draw(
         }],
     });
 
-    // TODO:
-    let swapchain_format = wgpu::TextureFormat::Rgba32Float; // or other format?
+    // TODO: which format to use?
+    let swapchain_format = wgpu::TextureFormat::Rgba32Float;
+
+    // TODO: this build step seems fairly messy, clean up?
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
+    let crate_path = [manifest_dir, "src"].iter().copied().collect::<PathBuf>();
+    let builder = SpirvBuilder::new(crate_path, "spirv-unknown-vulkan1.1")
+        .print_metadata(MetadataPrintout::None);
+
+    use spirv_builder::{CompileResult, MetadataPrintout, SpirvBuilder};
+    let initial_result = builder.build().unwrap();
+
+    fn handle_compile_result(
+        compile_result: CompileResult,
+    ) -> wgpu::ShaderModuleDescriptor<'static> {
+        let module_path = compile_result.module.unwrap_single();
+        let data = std::fs::read(module_path).unwrap();
+        let spirv = wgpu::util::make_spirv(&data);
+        let spirv = match spirv {
+            wgpu::ShaderSource::Wgsl(cow) => wgpu::ShaderSource::Wgsl(Cow::Owned(cow.into_owned())),
+            wgpu::ShaderSource::SpirV(cow) => {
+                wgpu::ShaderSource::SpirV(Cow::Owned(cow.into_owned()))
+            }
+        };
+        wgpu::ShaderModuleDescriptor {
+            label: None,
+            source: spirv,
+            flags: wgpu::ShaderFlags::default(),
+        }
+    }
+    let shader_binary = handle_compile_result(initial_result);
+
+    // TODO: what do we need for actually running the shader?
     let mut render_pipeline =
         create_pipeline(&device, &pipeline_layout, swapchain_format, shader_binary);
+    let mut sc_desc = wgpu::SwapChainDescriptor {
+        usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+        format: swapchain_format,
+        width: width,
+        height: height,
+        present_mode: wgpu::PresentMode::Mailbox,
+    };
+    let mut encoder =
+        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+    // TODO: i do not have a window and don't want one here
+    let mut surface = unsafe { instance.create_surface(&window) };
+    let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+    let frame = swap_chain
+        .get_current_frame()
+        .expect("Failed to acquire next swap chain texture")
+        .output;
+    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        label: None,
+        color_attachments: &[wgpu::RenderPassColorAttachment {
+            view: &frame.view,
+            resolve_target: None,
+            ops: wgpu::Operations {
+                load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
+                store: true,
+            },
+        }],
+        depth_stencil_attachment: None,
+    });
 
+    // TODO: placeholder constants
+    let time = 0.0;
+    let push_constants = ShaderConstants {
+        width,
+        height,
+        samples,
+        max_depth,
+        time,
+    };
+
+    rpass.set_pipeline(&render_pipeline);
+    rpass.set_push_constants(
+        wgpu::ShaderStage::all(),
+        0,
+        bytemuck::bytes_of(&push_constants),
+    );
+    rpass.draw(0..3, 0..1);
+    // TODO: return the actual results
+
+    // TODO: placeholder return
     let pixels = (width * height) as u64;
     let black = Color::new(0.0, 0.0, 0.0);
     let pixelbuffer: Vec<Color> = vec![black; pixels as usize];
