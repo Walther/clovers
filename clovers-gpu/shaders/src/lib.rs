5aeba2c4864cb0c2e4e843690fc2da26a5527415
Walther <veeti.haapsamo@gmail.com>
2021-08-22 18:08:26 +0300

    WIP: use a fullscreen triangle in vertex shader

--- clovers-gpu/shaders/src/lib.rs
+++ clovers-gpu/shaders/src/lib.rs
@@ -10,7 +10,7 @@
 // TODO: temporary during development
 #![allow(clippy::all)]
 
-use spirv_std::glam::{vec4, Vec4};
+use spirv_std::glam::{vec2, vec4, Vec2, Vec4};
 
 #[cfg(not(target_arch = "spirv"))]
 use spirv_std::macros::spirv;
@@ -23,14 +23,11 @@ pub fn main_fs(output: &mut Vec4) {
 
 #[spirv(vertex)]
 /// TODO:
-pub fn main_vs(
-    #[spirv(vertex_index)] vert_id: i32,
-    #[spirv(position, invariant)] out_pos: &mut Vec4,
-) {
-    *out_pos = vec4(
-        (vert_id - 1) as f32,
-        ((vert_id & 1) * 2 - 1) as f32,
-        0.0,
-        1.0,
-    );
+pub fn main_vs(#[spirv(vertex_index)] vert_idx: i32, #[spirv(position)] builtin_pos: &mut Vec4) {
+    // Create a "full screen triangle" by mapping the vertex index.
+    // ported from https://www.saschawillems.de/blog/2016/08/13/vulkan-tutorial-on-rendering-a-fullscreen-quad-without-buffers/
+    let uv = vec2(((vert_idx << 1) & 2) as f32, (vert_idx & 2) as f32);
+    let pos = 2.0 * uv - Vec2::ONE;
+
+    *builtin_pos = pos.extend(0.0).extend(1.0);
 }
