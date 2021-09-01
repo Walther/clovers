c2c04ccb5a84ba35af966fd423b5465187c4eafa
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2021-09-02 00:25:03 +0300

    Implement STL object, add Utah Teapot scene (#111)

--- clovers/src/objects.rs
+++ clovers/src/objects.rs
@@ -12,6 +12,8 @@ pub mod moving_sphere;
 pub mod quad;
 pub mod rotate;
 pub mod sphere;
+#[cfg(feature = "stl")]
+pub mod stl;
 pub mod translate;
 pub mod triangle;
 
@@ -23,6 +25,8 @@ pub use moving_sphere::*;
 pub use quad::*;
 pub use rotate::*;
 pub use sphere::*;
+#[cfg(feature = "stl")]
+pub use stl::*;
 pub use translate::*;
 pub use triangle::*;
 
@@ -41,16 +45,19 @@ pub enum Object {
     ConstantMedium(ConstantMediumInit),
     /// FlipFace object initializer
     FlipFace(FlipFaceInit),
-    /// ObjectList object initializer
-    ObjectList(ObjectList),
     /// MovingSphere object initializer
     MovingSphere(MovingSphere),
+    /// ObjectList object initializer
+    ObjectList(ObjectList),
     /// Quad object initializer
     Quad(QuadInit),
     /// RotateY object initializer
     RotateY(RotateInit),
     /// Sphere object initializer
     Sphere(SphereInit),
+    #[cfg(feature = "stl")]
+    /// STL object initializer
+    STL(STL),
     /// Translate object initializer
     Translate(TranslateInit),
     /// Triangle object initializer
@@ -71,16 +78,17 @@ impl From<Object> for Hitable {
                 let obj: Hitable = obj.into();
                 Hitable::FlipFace(FlipFace::new(obj))
             }
+            Object::MovingSphere(x) => Hitable::MovingSphere(MovingSphere::new(
+                x.center_0, x.center_1, x.time_0, x.time_1, x.radius, x.material,
+            )),
             Object::ObjectList(x) => {
+                // TODO: create a BVHNode instead?
                 let mut hitable_list = HitableList::new();
                 for obj in x {
                     hitable_list.add(obj.into())
                 }
                 Hitable::HitableList(hitable_list)
             }
-            Object::MovingSphere(x) => Hitable::MovingSphere(MovingSphere::new(
-                x.center_0, x.center_1, x.time_0, x.time_1, x.radius, x.material,
-            )),
             Object::Quad(x) => Hitable::Quad(Quad::new(x.q, x.u, x.v, x.material)),
             Object::RotateY(x) => {
                 let obj = *x.object;
@@ -88,6 +96,12 @@ impl From<Object> for Hitable {
                 Hitable::RotateY(RotateY::new(Box::new(obj), x.angle))
             }
             Object::Sphere(x) => Hitable::Sphere(Sphere::new(x.center, x.radius, x.material)),
+            #[cfg(feature = "stl")]
+            Object::STL(x) => {
+                // TODO: create a BVHNode instead?
+                let hitable_list: HitableList = x.into();
+                Hitable::HitableList(hitable_list)
+            }
             Object::Translate(x) => {
                 let obj = *x.object;
                 let obj: Hitable = obj.into();
---
+++ clovers/src/objects/stl.rs
@@ -0,0 +1,67 @@
+//! STL utilities
+// TODO: better docs
+
+use alloc::string::String;
+use nalgebra::Rotation3;
+use std::fs::OpenOptions;
+
+use crate::{
+    hitable::{Hitable, HitableList},
+    materials::Material,
+    objects::Triangle,
+    Float, Vec3,
+};
+
+/// STL structure. This gets converted into an internal representation using [Triangles](crate::objects::Triangle)
+#[derive(Debug)]
+#[cfg_attr(feature = "serde-derive", derive(serde::Serialize, serde::Deserialize))]
+pub struct STL {
+    /// Path of the .stl file
+    pub path: String,
+    /// Material to use for the .stl object
+    pub material: Material,
+    /// Scaling factor for the object
+    pub scale: Float,
+    /// Location of the object in the rendered scene
+    pub center: Vec3,
+    /// Rotation of the object. Described as three angles, `roll`, `pitch`, `yaw`, applied in that order.
+    pub rotation: Vec3,
+}
+
+impl From<STL> for HitableList {
+    fn from(stl: STL) -> Self {
+        // TODO: error handling!
+        let mut file = OpenOptions::new().read(true).open(stl.path).unwrap();
+        let mesh = stl_io::read_stl(&mut file).unwrap();
+        let triangles = mesh.vertices;
+        let mut hitable_list = HitableList::new();
+        for face in mesh.faces {
+            // TODO: verify if this is the correct order / makes sense / gets correct directions and normals
+            let a = triangles[face.vertices[0]];
+            let b = triangles[face.vertices[1]];
+            let c = triangles[face.vertices[2]];
+            // TODO: better conversion between library format and own format
+            let a: Vec3 = Vec3::new(a[0], a[1], a[2]);
+            let b: Vec3 = Vec3::new(b[0], b[1], b[2]);
+            let c: Vec3 = Vec3::new(c[0], c[1], c[2]);
+            // Handle rotation
+            let rotation = Rotation3::from_euler_angles(
+                stl.rotation[0].to_radians(),
+                stl.rotation[1].to_radians(),
+                stl.rotation[2].to_radians(),
+            );
+            let a: Vec3 = rotation * a;
+            let b: Vec3 = rotation * b;
+            let c: Vec3 = rotation * c;
+            // Handle scaling and offset
+            let a: Vec3 = a * stl.scale + stl.center;
+            let b: Vec3 = b * stl.scale + stl.center;
+            let c: Vec3 = c * stl.scale + stl.center;
+
+            let triangle = Triangle::from_coordinates(a, b, c, stl.material);
+            hitable_list.add(Hitable::Triangle(triangle));
+        }
+
+        hitable_list
+    }
+}
