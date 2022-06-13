504cd197065d21fed3b6e31fa7bf7f9d2f7f37e9
Walther <veeti.haapsamo@gmail.com>
2022-06-13 18:10:26 +0300

    WIP: gLTF: use a bit of metalness/roughness model, simplified

--- clovers-cli/src/gltf_scene.rs
+++ clovers-cli/src/gltf_scene.rs
@@ -2,7 +2,7 @@ use clovers::bvhnode::BVHNode;
 use clovers::camera::Camera;
 use clovers::color::Color;
 use clovers::hitable::Hitable;
-use clovers::materials::{DiffuseLight, Material};
+use clovers::materials::{DiffuseLight, Lambertian, Material, Metal};
 use clovers::objects::{Object, QuadInit, TriangleInit};
 use clovers::scenes::Scene;
 use clovers::textures::{SolidColor, Texture};
@@ -30,6 +30,17 @@ pub(crate) fn initialize(path: &Path, _opts: &Opts) -> Result<Scene, Box<dyn Err
     objects.push(lamp.clone());
     priority_objects.push(lamp);
 
+    // Add a floor for letting the light bounce a bit
+    let floor_mat =
+        Material::Lambertian(Lambertian::new(SolidColor::new(Color::new(0.7, 0.7, 0.7))));
+    let floor = Object::Quad(QuadInit {
+        q: Vec3::new(-100.0, -4.0, -100.0),
+        u: Vec3::new(200.0, 0.0, 0.0),
+        v: Vec3::new(0.0, 0.0, 200.0),
+        material: floor_mat,
+    });
+    objects.push(floor);
+
     // Go through the objects in the gltf file
     let (document, buffers, _images) = gltf::import(path)?;
     for scene in document.scenes() {
@@ -45,14 +56,32 @@ pub(crate) fn initialize(path: &Path, _opts: &Opts) -> Result<Scene, Box<dyn Err
                                     all_positions.push(vertex_position.into());
                                 }
                             }
-                            for x in all_positions.chunks_exact(3) {
-                                let material: Material = Default::default();
+
+                            let gltf_material = primitive.material();
+                            let roughness =
+                                gltf_material.pbr_metallic_roughness().roughness_factor();
+                            let metalness =
+                                gltf_material.pbr_metallic_roughness().metallic_factor();
+
+                            let material: Material = if metalness == 0.0 {
+                                Material::Lambertian(Lambertian::new(SolidColor::new(
+                                    Color::default(),
+                                )))
+                            } else {
+                                Material::Metal(Metal::new(
+                                    Texture::SolidColor(SolidColor::new(Color::default())),
+                                    roughness,
+                                ))
+                            };
+
+                            // Go through all the vertex positions in groups of three
+                            for (_index, triangle) in all_positions.chunks_exact(3).enumerate() {
                                 // NOTE: gLTF format uses absolute vertex positions for all three corners of the triangle
                                 // NOTE: internal representation uses triangle origin q, and relative u,v edge vectors
                                 let triangle = Object::Triangle(TriangleInit {
-                                    q: x[0],
-                                    u: x[1] - x[0],
-                                    v: x[2] - x[0],
+                                    q: triangle[0],
+                                    u: triangle[1] - triangle[0],
+                                    v: triangle[2] - triangle[0],
                                     material,
                                 });
                                 objects.push(triangle);
@@ -76,7 +105,7 @@ pub(crate) fn initialize(path: &Path, _opts: &Opts) -> Result<Scene, Box<dyn Err
 
     // Example hardcoded camera
     let camera = Camera::new(
-        Vec3::new(0.0, 0.0, 6.0),
+        Vec3::new(4.0, 0.0, 4.0),
         Vec3::new(0.0, 0.0, 0.0),
         Vec3::new(0.0, 1.0, 0.0),
         40.0,
