f8d7cfe4ee02b8ee1c16518a73218d5aa2052e1d
Walther <veeti.haapsamo@gmail.com>
2023-04-02 12:51:13 +0300

    refactor: initialize: return Scene instead of Box<Scene>

--- clovers-cli/src/json_scene.rs
+++ clovers-cli/src/json_scene.rs
@@ -10,7 +10,7 @@ use tracing::info;
 pub(crate) fn initialize<'scene>(
     path: &Path,
     opts: &Opts,
-) -> Result<Box<Scene<'scene>>, std::boxed::Box<dyn Error>> {
+) -> Result<Scene<'scene>, Box<dyn Error>> {
     let mut file = File::open(path)?;
     let mut contents: String = String::new();
     file.read_to_string(&mut contents)?;
@@ -19,5 +19,5 @@ pub(crate) fn initialize<'scene>(
     info!("Initializing the scene");
     let scene: Scene = scenes::initialize(scene_file, opts.width, opts.height);
     info!("Count of nodes in the BVH tree: {}", scene.objects.count());
-    Ok(Box::new(scene))
+    Ok(scene)
 }
