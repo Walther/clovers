2346c3bba7f7fc58fe9ec3c850fc526d4298dfa8
Walther <email+git@waltteri.net>
2024-02-03 18:08:17 +0100

    test: use more black_box in benches

--- clovers/benches/interval.rs
+++ clovers/benches/interval.rs
@@ -11,7 +11,7 @@ fn main() {
 fn new() -> Interval {
     let mut rng = SmallRng::from_entropy();
     let (a, b) = black_box((rng.gen(), rng.gen()));
-    Interval::new(a, b)
+    black_box(Interval::new(a, b))
 }
 
 #[divan::bench]
@@ -19,23 +19,23 @@ fn new_from_intervals() -> Interval {
     let mut rng = SmallRng::from_entropy();
     let (a, b) = black_box((rng.gen(), rng.gen()));
     let (c, d) = black_box((rng.gen(), rng.gen()));
-    let ab = Interval::new(a, b);
-    let cd = Interval::new(c, d);
-    Interval::new_from_intervals(ab, cd)
+    let ab = black_box(Interval::new(a, b));
+    let cd = black_box(Interval::new(c, d));
+    black_box(Interval::new_from_intervals(ab, cd))
 }
 
 #[divan::bench]
 fn expand() -> Interval {
     let mut rng = SmallRng::from_entropy();
     let (a, b) = black_box((rng.gen(), rng.gen()));
-    let ab = Interval::new(a, b);
-    ab.expand(rng.gen())
+    let ab = black_box(Interval::new(a, b));
+    black_box(ab.expand(rng.gen()))
 }
 
 #[divan::bench]
 fn size() -> Float {
     let mut rng = SmallRng::from_entropy();
     let (a, b) = black_box((rng.gen(), rng.gen()));
-    let ab = Interval::new(a, b);
-    ab.size()
+    let ab = black_box(Interval::new(a, b));
+    black_box(ab.size())
 }
