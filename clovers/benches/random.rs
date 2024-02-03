a95de2aa3c6fb1ff828942f16937010bf40a7897
Walther <email+git@waltteri.net>
2024-02-03 14:48:10 +0100

    test: switch from criterion to divan

--- clovers/benches/random.rs
+++ clovers/benches/random.rs
@@ -1,33 +1,33 @@
 use clovers::{random::*, Vec3};
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use divan::black_box;
 use rand::rngs::SmallRng;
-#[allow(unused_imports)]
 use rand::SeedableRng;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut rng = SmallRng::from_entropy();
-
-    c.bench_function("random in unit sphere", |b| {
-        b.iter(|| random_unit_vector(black_box(&mut rng)))
-    });
-
-    c.bench_function("random in unit disk", |b| {
-        b.iter(|| random_in_unit_disk(black_box(&mut rng)))
-    });
-
-    c.bench_function("random unit vector", |b| {
-        b.iter(|| random_unit_vector(black_box(&mut rng)))
-    });
-
-    c.bench_function("random cosine direction", |b| {
-        b.iter(|| random_cosine_direction(black_box(&mut rng)))
-    });
-
-    let normal = Vec3::new(1.0, 0.0, 0.0);
-    c.bench_function("random in hemisphere", |b| {
-        b.iter(|| random_on_hemisphere(normal, black_box(&mut rng)))
-    });
+fn main() {
+    divan::main();
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+#[divan::bench]
+fn unit_vector() -> Vec3 {
+    let mut rng = SmallRng::from_entropy();
+    random_unit_vector(black_box(&mut rng))
+}
+
+#[divan::bench]
+fn unit_disk() -> Vec3 {
+    let mut rng = SmallRng::from_entropy();
+    random_in_unit_disk(black_box(&mut rng))
+}
+
+#[divan::bench]
+fn cosine_direction() -> Vec3 {
+    let mut rng = SmallRng::from_entropy();
+    random_cosine_direction(black_box(&mut rng))
+}
+
+#[divan::bench]
+fn hemisphere() -> Vec3 {
+    let mut rng = SmallRng::from_entropy();
+    let normal = Vec3::new(1.0, 0.0, 0.0);
+    random_on_hemisphere(normal, black_box(&mut rng))
+}
