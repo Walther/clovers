9f5a4c4dc8a5392bf22ac0c606cc0235508ce618
Walther <email+git@waltteri.net>
2024-07-02 19:13:59 +0300

    chore: fix doc warn

--- clovers/src/lib.rs
+++ clovers/src/lib.rs
@@ -30,9 +30,9 @@
 //!
 //! - Rendering is done by creating [`Ray`](ray::Ray)s and seeing what they hit
 //! - A [`Ray`](ray::Ray) has an origin and a direction
-//! - Every [`Object`](objects::Object) has a `hit()` method that takes a [Ray](ray::Ray) and returns an Option<[`HitRecord`](HitRecord)>
+//! - Every [`Object`](objects::Object) has a `hit()` method that takes a [Ray](ray::Ray) and returns an Option<[`HitRecord`]>
 //! - If you get None, use that information to colorize your pixel with a default color
-//! - If you get Some([`HitRecord`](HitRecord)), use its details to colorize your pixel
+//! - If you get Some([`HitRecord`]), use its details to colorize your pixel
 //! - You most likely also want to recurse: depending on the material, maybe `scatter()` and cast a new [`Ray`](ray::Ray)?
 //!
 //! You most likely want to repeat this process multiple times for each of your pixels: generating multiple samples per pixel results in a higher quality image.
