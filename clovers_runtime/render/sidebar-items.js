3c2afe0af489cf34974479622e545e032d894c50
Walther <Walther@users.noreply.github.com>
2024-09-04 13:12:47 +0000

    deploy: 151d59f83dee8750beb09a89f426a1c09ca8abb3

---
+++ clovers_runtime/render/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["BvhAlgorithm","RenderMode"],"struct":["RenderOptions"]};
---
+++ trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"clovers_cli":[["impl Subcommand for <a class=\"enum\" href=\"clovers_cli/enum.Commands.html\" title=\"enum clovers_cli::Commands\">Commands</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
